9f64f53336112581524c22550c766edf0e013544
tgendrich <gendricht@gmail.com>
2021-08-12 14:49:19 -0500

    work

---
+++ best buy.sql
@@ -0,0 +1,13 @@
+#select * from products
+#select * from products where price > 1400
+#select * from products where price = 11.99 or price = 13.99
+#select * from products where not price = 11.99
+#select * from products order by(price) desc
+#select * from employees where MiddleInitial is null
+#SELECT distinct Name, Price FROM products 
+ #SELECT * FROM employees WHERE FirstName LIKE 'j%'
+ #SELECT * FROM products where Name = 'Macbook'
+ #select * from products WHERE OnSale = 1
+ #SELECT AVG(price) from products
+ #SELECT MiddleInitial, Title, FirstName,LastName from employees where Title = 'Geek Squad' and MiddleInitial is null
+ #select * from products where StockLevel between 500 and 1200 
